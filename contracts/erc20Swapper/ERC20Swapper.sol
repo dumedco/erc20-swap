3124ab589e1d4841dbc244e2b286e5b52e200f48
Eduard Dumea <ec.dumea@gmail.com>
2024-03-02 10:19:09 +0200

    project init

---
+++ contracts/erc20Swapper/ERC20Swapper.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.24;
+
+interface ERC20Swapper {
+    /// @dev swaps the `msg.value` Ether to at least `minAmount` of tokens in `address`, or reverts
+    /// @param token The address of ERC-20 token to swap
+    /// @param minAmount The minimum amount of tokens transferred to msg.sender
+    /// @return The actual amount of transferred tokens
+    function swapEtherToToken(address token, uint minAmount) public payable returns (uint) {
+        return 2;
+    }
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,51 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+};
+module.exports = {
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.MAINNET_URL || "",
+      }
+    },
+    mainnet: {
+      url: process.env.MAINNET_URL || "",
+      accounts:
+        process.env.DEPLOYER_PRIVATE_KEY !== undefined ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_URL || "",
+      gasPrice: 'auto',
+      accounts:
+        process.env.DEPLOYER_PRIVATE_KEY !== undefined ? [process.env.DEPLOYER_PRIVATE_KEY] : [],
+    },
+  },
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 40000
+  }
+}
+
+
+export default config;
